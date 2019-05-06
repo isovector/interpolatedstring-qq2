999f02c4cac73479b2a4dfbff8c88dcb2816f4b7
Sandy Maguire <sandy@sandymaguire.me>
2019-05-06 14:57:05 -0300

    Updated

---
+++ src/Text/InterpolatedString/QQ2.hs
@@ -0,0 +1,165 @@
+{-# LANGUAGE TemplateHaskell, TypeSynonymInstances, FlexibleInstances,
+  UndecidableInstances, MultiParamTypeClasses #-}
+
+-- | QuasiQuoter for interpolated strings using Perl 6 syntax.
+--
+-- The 'q' form does one thing and does it well: It contains a multi-line string with
+-- no interpolation at all:
+--
+-- @
+-- {-\# LANGUAGE QuasiQuotes, ExtendedDefaultRules #-}
+-- import Text.InterpolatedString.QQ2 (q)
+-- foo :: String -- 'Text', 'ByteString' etc also works
+-- foo = [q|
+--
+-- Well here is a
+--     multi-line string!
+--
+-- |]
+-- @
+--
+-- Any instance of the 'IsString' class is permitted.
+--
+-- The 'qc' form interpolates curly braces: expressions inside {} will be
+-- directly interpolated if it's a 'Char', 'String', 'Text' or 'ByteString', or
+-- it will have 'show' called if it is not.
+--
+-- Escaping of '{' is done with backslash.
+--
+-- For interpolating numeric expressions without an explicit type signature,
+-- use the ExtendedDefaultRules lanuage pragma, as shown below:
+--
+-- @
+-- {-\# LANGUAGE QuasiQuotes, ExtendedDefaultRules #-}
+-- import Text.InterpolatedString.QQ2 (qc)
+-- bar :: String
+-- bar = [qc| Well {\"hello\" ++ \" there\"} {6 * 7} |]
+-- @
+--
+-- bar will have the value \" Well hello there 42 \".
+--
+-- If you want control over how 'show' works on your types, define a custom
+-- 'ShowQ' instance:
+--
+-- For example, this instance allows you to display interpolated lists of strings as
+-- a sequence of words, removing those pesky brackets, quotes, and escape sequences.
+--
+-- @
+-- {-\# LANGUAGE FlexibleInstances #-}
+-- import Text.InterpolatedString.QQ2 (qc, ShowQ(..))
+-- instance ShowQ [String] where
+--     showQ = unwords
+-- @
+--
+-- 'qc' permits output to any types with both 'IsString' and 'Monoid'
+-- instances.
+--
+-- @
+-- {-# LANGUAGE QuasiQuotes, OverloadedStrings #-}
+-- import Text.InterpolatedString.QQ2 (qc)
+-- import Data.Text (Text)
+-- import Data.ByteString.Char8 (ByteString)
+-- qux :: ByteString
+-- qux = [qc| This will convert {\"Text\" :: Text} to {\"ByteString\" :: ByteString} |]
+-- @
+
+module Text.InterpolatedString.QQ2 (qc, q, ShowQ(..)) where
+
+import           Data.ByteString.Char8 as Strict (ByteString, unpack)
+import           Data.ByteString.Lazy.Char8 as Lazy (ByteString, unpack)
+import           Data.Monoid (Monoid(..))
+import           Data.Text as T (Text, unpack)
+import           Data.Text.Lazy as LazyT(Text, unpack)
+import           GHC.Exts (IsString(..))
+import           Language.Haskell.Meta.Parse
+import qualified Language.Haskell.TH as TH
+import           Language.Haskell.TH.Quote
+
+-- |A class for types that use special interpolation rules.
+-- Instances of 'ShowQ' that are also instances of 'IsString' should obey the
+-- following law:
+--
+-- @
+-- fromString (showQ s) == s
+-- @
+--
+-- because this library relies on this fact to optimize
+-- away needless string conversions.
+class ShowQ a where
+    showQ :: a -> String
+
+instance ShowQ Char where
+    showQ = (:[])
+
+instance ShowQ String where
+    showQ = id
+
+instance ShowQ Strict.ByteString where
+    showQ = Strict.unpack
+
+instance ShowQ Lazy.ByteString where
+    showQ = Lazy.unpack
+
+instance ShowQ T.Text where
+    showQ = T.unpack
+
+instance ShowQ LazyT.Text where
+    showQ = LazyT.unpack
+
+instance {-# OVERLAPPABLE #-} Show a => ShowQ a where
+    showQ = show
+
+-- todo: this should really be rewritten into RULES pragmas, but so far
+-- I can't convince GHC to let the rules fire.
+class QQ a string where
+    toQQ :: a -> string
+
+instance {-# INCOHERENT #-} IsString s => QQ s s where
+    toQQ = id
+
+instance {-# INCOHERENT #-} (ShowQ a, IsString s) => QQ a s where
+    toQQ = fromString . showQ
+
+data StringPart = Literal String | AntiQuote String deriving Show
+
+unQC :: [Char] -> [Char] -> [StringPart]
+unQC a []          = [Literal (reverse a)]
+unQC a ('\\':x:xs) = unQC (x:a) xs
+unQC a ('\\':[])   = unQC ('\\':a) []
+unQC a ('}':xs)    = AntiQuote (reverse a) : parseQC [] xs
+unQC a (x:xs)      = unQC (x:a) xs
+
+parseQC :: [Char] -> [Char] -> [StringPart]
+parseQC a []             = [Literal (reverse a)]
+parseQC a ('\\':'\\':xs) = parseQC ('\\':a) xs
+parseQC a ('\\':'#':xs)  = parseQC ('#':a) xs
+parseQC a ('\\':[])      = parseQC ('\\':a) []
+parseQC a ('#':'{':xs)   = Literal (reverse a) : unQC [] xs
+parseQC a (x:xs)         = parseQC (x:a) xs
+
+makeExpr :: [StringPart] -> TH.Q TH.Exp
+makeExpr [] = [| mempty |]
+makeExpr ((Literal a):xs)   = TH.appE [| mappend (fromString a) |]
+                              $ makeExpr xs
+makeExpr ((AntiQuote a):xs) = TH.appE [| mappend (toQQ $(reify a)) |]
+                              $ makeExpr xs
+
+reify :: String -> TH.Q TH.Exp
+reify s =
+    case parseExp s of
+        Left s'  -> TH.reportError s' >> [| mempty |]
+        Right e ->  return e
+
+-- | QuasiQuoter for interpolating '{expr}' into a string literal. The pattern portion is undefined.
+qc :: QuasiQuoter
+qc = QuasiQuoter (makeExpr . parseQC [] . filter (/= '\r'))
+                 (error "Cannot use qc as a pattern")
+                 (error "Cannot use qc as a type")
+                 (error "Cannot use qc as a dec")
+
+-- | QuasiQuoter for a non-interpolating string literal. The pattern portion is undefined.
+q :: QuasiQuoter
+q = QuasiQuoter ((\a -> [|fromString a|]) . filter (/= '\r'))
+                 (error "Cannot use q as a pattern")
+                 (error "Cannot use q as a type")
+                 (error "Cannot use q as a dec")
--- tests/Test.hs
+++ tests/Test.hs
@@ -1,43 +1,53 @@
-{-# LANGUAGE QuasiQuotes, ExtendedDefaultRules #-}
+{-# LANGUAGE QuasiQuotes, ExtendedDefaultRules, OverloadedStrings, IncoherentInstances #-}
 
 module Main where
 
-import Text.InterpolatedString.Perl6
-import Test.HUnit
-import GHC.Exts(fromString)
 import Data.ByteString.Char8 as BS(ByteString, pack)
 import Data.Text as T(Text, pack)
+import GHC.Exts(fromString)
+import Test.HUnit
+import Text.InterpolatedString.QQ2
 
 data Foo = Foo Int String deriving Show
 
+t1 :: String
 t1 = "字元"
 
-testEmpty       = assertBool "" ([$qc||] == "")
-testCharLiteral = assertBool "" ([$qc|{1+2}|] == "3")
-testString      = assertBool "" ([$qc|a string {t1} is here|] == "a string 字元 is here")
-testVariable    = assertBool "" ([$qq|a string {t1} $t1 {t1} $t1 is here|] == "a string 字元 字元 字元 字元 is here")
-testEscape      = assertBool "" ([$qc|#\{}|] == "#{}" && [$qc|\{}|] == "{}")
-testComplex     = assertBool "" ([$qc|
+testEmpty       = assertBool "" ([qc||] == "")
+testCharLiteral = assertBool "" ([qc|#{1+2}|] == "3")
+testString      = assertBool "" ([qc|a string #{t1} is here|] == "a string 字元 is here")
+testEscape      = assertBool "" ([qc|\#{ }|] == "#{ }")
+testComplex     = assertBool "" ([qc|
         \ok
-{Foo 4 "Great!" : [Foo 3 "Scott!"]}
+#{Foo 4 "Great!" : [Foo 3 "Scott!"]}
         then
 |] == ("\n" ++
     "        \\ok\n" ++
     "[Foo 4 \"Great!\",Foo 3 \"Scott!\"]\n" ++
     "        then\n"))
-testConvert = assertBool "" 
-              (([$qc|{fromString "a"::Text} {fromString "b"::ByteString}|] :: String)
+testConvert = assertBool ""
+              (([qc|#{fromString "a"::Text} #{fromString "b"::ByteString}|] :: String)
                == "a b")
 
 tests = TestList
-    [ TestLabel "Empty String" $ TestCase testEmpty
-    , TestLabel "Character Literal" $ TestCase testCharLiteral
-    , TestLabel "String Variable" $ TestCase testString
-    , TestLabel "Dollar Variable" $ TestCase testVariable
-    , TestLabel "Escape Sequences" $ TestCase testEscape
+    [ TestLabel "Empty String"       $ TestCase testEmpty
+    , TestLabel "Character Literal"  $ TestCase testCharLiteral
+    , TestLabel "String Variable"    $ TestCase testString
+    , TestLabel "Escape Sequences"   $ TestCase testEscape
     , TestLabel "Complex Expression" $ TestCase testComplex
-    , TestLabel "String Conversion" $ TestCase testConvert
+    , TestLabel "String Conversion"  $ TestCase testConvert
+    , TestLabel "ByteString Test"    $ TestCase testByteString
+    , TestLabel "Text Test"          $ TestCase testText
     ]
 
 main = runTestTT tests
+
+
+-- the primary purpose of these tests is to ensure that
+-- the Text and ByteString rewrite rules are firing, to avoid
+-- needlessly converting string types
+testByteString = assertBool "" $ [qc|#{"a" :: ByteString} #{"b" :: ByteString}|]
+                 == BS.pack ("a b")
+testText = assertBool "" $ [qc|#{"a" :: Text} #{"b" :: Text}|]
+           == T.pack ("a b")
 
